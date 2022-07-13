1e4585a67b8cd704a79318852cb53258f8c75331
evermine18 <edgarsanou@gmail.com>
2022-07-13 11:25:46 +0200

    Train render, with coords system and movement, key events that change train speed

--- engine/game_engine.py
+++ engine/game_engine.py
@@ -5,28 +5,34 @@ import os
 colliders=[]
 
 class GameObject(pg.sprite.Sprite):
-    name=""
-    x=0
-    y=0
-    renderObj=""
+
     def __init__(self,name,sprite_dir,x=0,y=0):
         super(GameObject,self).__init__()
         self.name=name
         self.x=x
         self.y=y
+        self.speed=0
         self.image=pg.image.load(os.path.join("sprites",sprite_dir[0],sprite_dir[1]))
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
+
     def getCoords(self):
         print("Coords from gameObject ",self.name," x=",self.x," y=",self.y)
+
+    def getSpeed(self):
+        return self.speed
+
+    def increaseSpeed(self):
+        self.speed+=1
+
+    def decreaseSpeed(self):
+        self.speed-=1
+
     def getRenderObj(self):
         return self.renderObj
-class RenderObject(pg.sprite.Sprite):
-    sprite=""
-    def __init__(self,sprite_loc,x,y):
-        super(self).__init__()
-        self.image=pg.image.load(os.path.join("sprites",sprite_loc[0], sprite_loc[1]))
-        self.rect = self.image.get_rect()
-        self.rect.center = (x,y)
+
+    def update(self):
+        self.rect.x = self.rect.x + self.speed
+
 
     
--- main.py
+++ main.py
@@ -4,36 +4,42 @@ from engine import *
 from engine.game_engine import GameObject
 
 pg.init()
-
-# Set up the drawing window
+clock = pg.time.Clock()
+clock.tick(60)
+# Set up the drawing window with name and res
 screen = pg.display.set_mode([500, 500])
-x=255
+pg.display.set_caption("Train Driver")
+#Test GameObject
 tren=GameObject("Tren",["objects","train.png"],500,450)
 gameObjs= pg.sprite.RenderPlain()
 gameObjs.add(tren)
+#DEBUG VARS
+font = pg.font.SysFont(None, 24)
+GREEN = (0, 255, 0)
 # Run until the user asks to quit
 running = True
 while running:
-
-    # Did the user click the window close button?
+    #Keyboard events
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_LEFT:
-                x -= 1
-            if event.key == pg.K_RIGHT:
-                x += 1
+            if event.key == pg.K_d:
+                tren.increaseSpeed()
+            if event.key == pg.K_a:
+                tren.decreaseSpeed()
     # Screen print 
     screen.fill((255, 255, 255))
     #tren.getCoords()
     gameObjs.draw(screen)
     gameObjs.update()
-    # Draw a solid blue circle in the center
-    #pygame.draw.circle(screen, (0, 0, 255), (x, 250), 75)
-    #x += 1
+    #Draw DEBUG Info
+    fps_count = font.render('FPS: '+str(clock.get_fps()), True, GREEN)
+    screen.blit(fps_count, (20, 20))
+    train_speed = font.render(' Train Speed: '+str(tren.getSpeed()), True, GREEN)
+    screen.blit(train_speed, (15, 50))
     # Flip the display
     pg.display.flip()
-
+    clock.tick(60)
 # Done! Time to quit.
 pg.quit()
